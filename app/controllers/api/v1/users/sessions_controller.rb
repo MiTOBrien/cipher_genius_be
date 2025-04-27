5d85bfc123f4a5a4c1f8904d72ecebede317ad12
Michael OBrien <obrien.michael.t@gmail.com>
2025-04-27 11:59:44 -0400

    sets up devise for acct creatation and login

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,17 +1,43 @@
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+module Api
+  module V1
+    module Users
+      class SessionsController < Devise::SessionsController
+        respond_to :json
 
-  private
+        # 🛠 Add this create method
+        def create
+          user = User.find_by(email: sign_in_params[:email])
 
-  def respond_with(resource, _opts = {})
-    render json: { message: 'Logged in successfully.', user: resource }, status: :ok
-  end
+          if user&.valid_password?(sign_in_params[:password])
+            sign_in(:user, store: false)
+            render json: {
+              message: 'Logged in successfully.',
+              user: UserSerializer.new(user).serializable_hash[:data][:attributes]
+            }, status: :ok
+          else
+            render json: { error: 'Invalid Email or Password' }, status: :unauthorized
+          end
+        end
 
-  def respond_to_on_destroy
-    if current_user
-      render json: { message: "Logged out successfully." }, status: :ok
-    else
-      render json: { message: "Couldn't find an active session." }, status: :unauthorized
+        private
+
+        def respond_with(resource, _opts = {})
+          render json: {
+            message: 'Logged in successfully.',
+            user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+          }, status: :ok
+        end
+
+        def respond_to_on_destroy
+          current_user ? 
+            render(json: { message: "Logged out successfully." }, status: :ok) :
+            render(json: { message: "Couldn't find an active session." }, status: :unauthorized)
+        end
+
+        def sign_in_params
+          params.permit(:email, :password)
+        end
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  
   before_action :authenticate_user!, except: [:create, :new]
+  
   respond_to :json
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 end
