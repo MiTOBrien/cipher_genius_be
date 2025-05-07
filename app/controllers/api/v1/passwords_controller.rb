df6d1caa7246315932ec504e89cbd47d4e54c159
Michael OBrien <obrien.michael.t@gmail.com>
2025-05-07 08:03:11 -0400

    enables BE for forgot or reset pwd requests

---
+++ app/controllers/api/v1/passwords_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::PasswordsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present?
+      user.send_reset_password_instructions
+      render json: { message: 'Password reset instructions sent.' }, status: :ok
+    else
+      render json: { error: 'Email not found' }, status: :not_found
+    end
+  end
+
+  def update
+    user = User.reset_password_by_token(reset_password_params)
+    if user.errors.empty?
+      render json: { message: 'Password has been reset.' }, status: :ok
+    else
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reset_password_params
+    params.permit(:reset_password_token, :password, :password_confirmation)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3001 }
+  # Looking to send emails in production? Check out our Email API/SMTP product!
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  user_name: '5a3c384a482f4f',
+  password: 'fca97276c238df',
+  address: 'sandbox.smtp.mailtrap.io',
+  host: 'sandbox.smtp.mailtrap.io',
+  port: '2525',
+  authentication: :login
+}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
