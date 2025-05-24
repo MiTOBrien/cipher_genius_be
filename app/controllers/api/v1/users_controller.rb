66bc61bb916deec8df18feee69c2f50bf259d24d
Michael OBrien <obrien.michael.t@gmail.com>
2025-05-24 09:35:02 -0400

    Adds change password functionality to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,9 +9,32 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def change_password
+    # Validate current password
+    unless current_user.valid_password?(password_change_params[:current_password])
+      return render json: { error: 'Current password is incorrect.' }, status: :unauthorized
+    end
+
+    # Validate new password confirmation
+    if password_change_params[:new_password] != password_change_params[:new_password_confirmation]
+      return render json: { error: 'New password confirmation does not match.' }, status: :unprocessable_entity
+    end
+
+    # Update password
+    if current_user.update(password: password_change_params[:new_password])
+      render json: { message: 'Password changed successfully.' }, status: :ok
+    else
+      render json: { error: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :username)
+  end
+
+  def password_change_params
+    params.permit(:current_password, :new_password, :new_password_confirmation)
   end
 end
